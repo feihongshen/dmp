91c9b2aa7d1ba2e87d8f01e8fa98fa3a50cb79c4
liuxiao <liuxiao@pjbest.com>
2015-01-07 03:31:13 +0000

    【修改】通联POS对接，修改Delivery【刘肖】

--- src/main/java/cn/explink/pos/tonglianpos/delivery/Delivery.java
+++ src/main/java/cn/explink/pos/tonglianpos/delivery/Delivery.java
@@ -3,34 +3,58 @@ package cn.explink.pos.tonglianpos.delivery;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name="request")
+@XmlRootElement(name = "request")
 public class Delivery {
-	
-	private String delivery_man;//物流员工号，登录用户名
-	private String delivery_dept_no;//员工所在站点编辑
-	private String order_no;//扫描请求单号
-	
-	@XmlElement(name="delivery_man")
-	public String getDelivery_man() {
-		return delivery_man;
+
+	private String delivery_man;// 物流员工号，登录用户名
+	private String delivery_dept_no;// 员工所在站点编辑
+	private String order_no;// 扫描请求单号
+	private String sign;
+	private String requestTime;
+
+	@XmlElement(name = "sign")
+	public String getSign() {
+		return this.sign;
 	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	@XmlElement(name = "requestTime")
+	public String getRequestTime() {
+		return this.requestTime;
+	}
+
+	public void setRequestTime(String requestTime) {
+		this.requestTime = requestTime;
+	}
+
+	@XmlElement(name = "delivery_man")
+	public String getDelivery_man() {
+		return this.delivery_man;
+	}
+
 	public void setDelivery_man(String delivery_man) {
 		this.delivery_man = delivery_man;
 	}
-	
-	@XmlElement(name="delivery_dept_no")
+
+	@XmlElement(name = "delivery_dept_no")
 	public String getDelivery_dept_no() {
-		return delivery_dept_no;
+		return this.delivery_dept_no;
 	}
+
 	public void setDelivery_dept_no(String delivery_dept_no) {
 		this.delivery_dept_no = delivery_dept_no;
 	}
-	@XmlElement(name="order_no")
+
+	@XmlElement(name = "order_no")
 	public String getOrder_no() {
-		return order_no;
+		return this.order_no;
 	}
+
 	public void setOrder_no(String order_no) {
 		this.order_no = order_no;
 	}
-	
+
 }
