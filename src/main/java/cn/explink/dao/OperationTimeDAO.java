30110b109c4ad352e05937a1789a2e8950798d36
lans <lans@pjbest.com>
2015-03-16 19:06:02 +0000

    【修改】订单生命周期监控，完成提交 【蓝生】

--- src/main/java/cn/explink/dao/OperationTimeDAO.java
+++ src/main/java/cn/explink/dao/OperationTimeDAO.java
@@ -55,10 +55,10 @@ public class OperationTimeDAO {
 	 * @param of
 	 * @return key
 	 */
-	public void creOperationTime(String cwb, long branchid, int flowordertype, long deliverystate, long nextbranchid, long customerid, String inwarehouseTime, String emaildate,int cwbordertypeid) {
+	public void creOperationTime(String cwb, long branchid, int flowordertype, long deliverystate, long nextbranchid, long customerid, String inwarehouseTime, String emaildate,int cwbordertypeid,BigDecimal receivablefee, BigDecimal paybackfee) {
 		this.jdbcTemplate.update(
-				"insert into express_ops_operation_time(cwb,branchid,credate,flowordertype,deliverystate,nextbranchid,customerid,outwarehouseTime,emaildate,cwbordertypeid) values(?,?,?,?,?,?,?,?,?,?)", cwb, branchid,
-				System.currentTimeMillis(), flowordertype, deliverystate, nextbranchid, customerid, inwarehouseTime, emaildate,cwbordertypeid);
+				"insert into express_ops_operation_time(cwb,branchid,credate,flowordertype,deliverystate,nextbranchid,customerid,outwarehouseTime,emaildate,cwbordertypeid) values(?,?,?,?,?,?,?,?,?,?,?,?)", cwb, branchid,
+				System.currentTimeMillis(), flowordertype, deliverystate, nextbranchid, customerid, inwarehouseTime, emaildate,cwbordertypeid,receivablefee,paybackfee);
 	}
 
 	public void creAndUpdateOperationTime(String cwb, long branchid, int flowordertype, long deliverystate, long nextbranchid, 
@@ -73,7 +73,7 @@ public class OperationTimeDAO {
 				this.updateOperationTime(cwb, branchid, flowordertype, deliverystate, nextbranchid);
 			}
 		} else {
-			this.creOperationTime(cwb, branchid, flowordertype, deliverystate, nextbranchid, customeid, inwarehouseTime, emaildate,cwbordertypeid);
+			this.creOperationTime(cwb, branchid, flowordertype, deliverystate, nextbranchid, customeid, inwarehouseTime, emaildate,cwbordertypeid,receivablefee,paybackfee);
 		}
 	}
 
