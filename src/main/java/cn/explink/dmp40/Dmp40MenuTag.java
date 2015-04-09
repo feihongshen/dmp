70426e270d7ea6dd6bb542b40e3bcba96aa50949
lans <lans@pjbest.com>
2015-04-09 12:35:10 +0000

    【修改】菜单替换oms站位符 【蓝生】

--- src/main/java/cn/explink/dmp40/Dmp40MenuTag.java
+++ src/main/java/cn/explink/dmp40/Dmp40MenuTag.java
@@ -3,6 +3,7 @@ package cn.explink.dmp40;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.servlet.ServletContext;
 import javax.servlet.jsp.JspTagException;
@@ -10,12 +11,17 @@ import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.PropertyPlaceholderHelper;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import cn.explink.dao.SystemInstallDAO;
+import cn.explink.domain.SystemInstall;
 
 public class Dmp40MenuTag extends TagSupport {
 
 	private static final long serialVersionUID = 1L;
-
+	static PropertyPlaceholderHelper placeholderHelper = new PropertyPlaceholderHelper("${", "}");
+	
 	public int doStartTag() throws JspTagException {
 		return EVAL_PAGE;
 	}
@@ -32,22 +38,26 @@ public class Dmp40MenuTag extends TagSupport {
 	}
 
 	public StringBuffer end() {
+		
+		String dmpid = "dmpid=" + this.pageContext.getSession().getId();
+		String httppath = "http://" + this.pageContext.getRequest().getLocalAddr()+":"+this.pageContext.getRequest().getLocalPort();
 
 		ServletContext sc = this.pageContext.getServletContext();
 		ApplicationContext ac2 = WebApplicationContextUtils
 				.getWebApplicationContext(sc);
 		Dmp40FunctionDAO dmp40FunctionDAO = ac2.getBean(Dmp40FunctionDAO.class);
-
+		Properties properties = getSystemInstallProperties();
+		
 		StringBuffer sb = new StringBuffer();
 		sb.append("<div id=\"nav\" style=\"display:none;\" class=\"easyui-accordion\" fit=\"true\" border=\"false\">");
-		sb.append(Dmp40MenuTag.getEasyuiMultistageTree(dmp40FunctionDAO
-				.getFunctionMap()));
+		sb.append(Dmp40MenuTag.getEasyuiMultistageTree(
+				dmp40FunctionDAO.getFunctionMap(), dmpid,properties,httppath));
 		sb.append("</div>");
 		return sb;
 	}
 
 	public static String getEasyuiMultistageTree(
-			Map<Integer, List<Dmp40Function>> map) {
+			Map<Integer, List<Dmp40Function>> map, String dmpid,Properties properties,String httppath) {
 		if (map == null || map.size() == 0 || !map.containsKey(0)) {
 			return "不具有任何权限,\n请找管理员分配权限";
 		}
@@ -65,7 +75,7 @@ public class Dmp40MenuTag extends TagSupport {
 				menuString
 						.append("<ul class=\"easyui-tree\"  fit=\"false\" border=\"false\">");
 			}
-			menuString.append(getChildOfTree(function, 1, map));
+			menuString.append(getChildOfTree(function, 1, map, dmpid, properties,httppath));
 			if (submenusize > 0) {
 				menuString.append("</ul></div>");
 			}
@@ -74,29 +84,46 @@ public class Dmp40MenuTag extends TagSupport {
 	}
 
 	private static String getChildOfTree(Dmp40Function parent, int level,
-			Map<Integer, List<Dmp40Function>> map) {
+			Map<Integer, List<Dmp40Function>> map, String dmpid,Properties properties,String httppath) {
 		StringBuffer menuString = new StringBuffer();
 		List<Dmp40Function> list = map.get(level);
 		for (Dmp40Function function : list) {
 			if (function.getDmp40Function().getId().equals(parent.getId())) {
 				if (function.getDmp40FunctionList().size() == 0
 						|| !map.containsKey(level + 1)) {
-					menuString.append(getLeafOfTree(function));
+					menuString.append(getLeafOfTree(function, dmpid, properties,httppath));
 				} else if (map.containsKey(level + 1)) {
 					menuString.append("<li state=\"closed\" iconCls=\""
 							+ "\" ><span>" + function.getFunctionName()
 							+ "</span>");
 					menuString.append("<ul >");
-					menuString.append(getChildOfTree(function, level + 1, map));
+					menuString.append(getChildOfTree(function, level + 1, map,
+							dmpid,properties,httppath));
 					menuString.append("</ul></li>");
 				}
 			}
 		}
 		return menuString.toString();
 	}
-
-	private static String getLeafOfTree(Dmp40Function function) {
-
+	
+	private  Properties getSystemInstallProperties() {
+		ServletContext sc = this.pageContext.getServletContext();
+		ApplicationContext ac2 = WebApplicationContextUtils
+				.getWebApplicationContext(sc);
+		SystemInstallDAO systemInstallDAO = ac2.getBean(SystemInstallDAO.class);
+		List<SystemInstall> systemInstalls = systemInstallDAO.getAllProperties();
+		Properties properties = new Properties();
+		for (SystemInstall systemInstall : systemInstalls) {
+			properties.put(systemInstall.getName(), systemInstall.getValue());
+		}
+		return properties;
+	}
+	
+	private static String getLeafOfTree(Dmp40Function function, String dmpid,Properties properties,String httppath) {
+		String menuurl = function.getFunctionUrl();
+		if(function.getFunctionUrl().indexOf("{") >-1){
+			menuurl =httppath + placeholderHelper.replacePlaceholders(function.getFunctionUrl(), properties);
+		}
 		StringBuffer menuString = new StringBuffer();
 		String icon = "folder";
 		menuString.append("<li iconCls=\"");
@@ -104,7 +131,7 @@ public class Dmp40MenuTag extends TagSupport {
 		menuString.append("\"> <a onclick=\"addTab(\'");
 		menuString.append(function.getFunctionName());
 		menuString.append("\',\'");
-		menuString.append(function.getFunctionUrl());
+		menuString.append(menuurl + dmpid);
 		menuString.append("&clickFunctionId=");
 		menuString.append(function.getId());
 		menuString.append("\',\'");
@@ -112,7 +139,7 @@ public class Dmp40MenuTag extends TagSupport {
 		menuString.append("\')\"  title=\"");
 		menuString.append(function.getFunctionName());
 		menuString.append("\" url=\"");
-		menuString.append(function.getFunctionUrl());
+		menuString.append(menuurl + dmpid);
 		menuString.append("\" href=\"#\" ><span class=\"nav\" >");
 		menuString.append(function.getFunctionName());
 		menuString.append("</span></a></li>");
