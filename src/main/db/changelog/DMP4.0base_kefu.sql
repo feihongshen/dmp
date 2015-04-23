11711648e5df2fd0a6416ad63da3b0cb6c97ac08
zpk <zpk@pjbest.com>
2015-04-23 13:41:00 +0000

    【修改】新增客服sql代码 张鹏凯

--- src/main/db/changelog/DMP4.0base_kefu.sql
+++ src/main/db/changelog/DMP4.0base_kefu.sql
@@ -15,9 +15,7 @@ MySQL - 5.1.62-community : Database - dmp4_train
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dmp4.1_train` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `dmp4.1_train`;
 
 /*Table structure for table `account_bill` */
 
