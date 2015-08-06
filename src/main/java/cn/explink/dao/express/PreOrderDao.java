eb12b88e9435a8c632dba646043a3970ea64dcd0
liuwuqiang <liuwuqiang@pjbest.com>
2015-08-06 08:11:18 +0000

    提交预订单查询功能的dao方法

--- src/main/java/cn/explink/dao/express/PreOrderDao.java
+++ src/main/java/cn/explink/dao/express/PreOrderDao.java
@@ -1,10 +1,15 @@
 package cn.explink.dao.express;
 
+import java.io.Serializable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -12,6 +17,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import cn.explink.domain.VO.PreOrderQueryDateVO;
 import cn.explink.domain.express.ExpressPreOrder;
 import cn.explink.util.Page;
 
@@ -22,9 +28,152 @@ import cn.explink.util.Page;
  */
 
 @Component
-public class PreOrderDao {
+public class PreOrderDao implements Serializable {
+	/**
+	 * @description TODO
+	 * @author 刘武强
+	 * @date  2015年8月4日上午10:38:33
+	 */
+	private static final long serialVersionUID = -3711933464597436655L;
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+
+	/**
+	 *
+	 * @description 将预订单查询结果转化为实体对象的RowMapper(全部字段)由于和志宇的冲突，暂时注释
+	 * @author  刘武强
+	 * @data   2015年8月3日
+	 */
+	/*private final class ExpressPreOrderRowMapper implements RowMapper<ExpressPreOrder> {
+		@Override
+		public ExpressPreOrder mapRow(ResultSet rs, int rowNum) throws SQLException {
+			ExpressPreOrder expressPreOrder = new ExpressPreOrder();
+			expressPreOrder.setId(rs.getInt("id"));
+			expressPreOrder.setPreOrderNo(rs.getString("pre_order_no"));
+			expressPreOrder.setStatus(rs.getInt("status"));
+			expressPreOrder.setExcuteState(rs.getInt("excute_state"));
+			expressPreOrder.setSendPerson(rs.getString("send_person"));
+			expressPreOrder.setCellphone(rs.getString("cellphone"));
+			expressPreOrder.setTelephone(rs.getString("telephone"));
+			expressPreOrder.setCollectAddress(rs.getString("collect_address"));
+			expressPreOrder.setReason(rs.getString("reason"));
+			expressPreOrder.setBranchId(rs.getInt("branch_id"));
+			expressPreOrder.setBranchName(rs.getString("branch_name"));
+			expressPreOrder.setHandleTime(rs.getDate("handle_time"));
+			expressPreOrder.setHandleUserId(rs.getInt("handle_user_id"));
+			expressPreOrder.setHandleUserName(rs.getString("handle_user_name"));
+			expressPreOrder.setDistributeDeliverman_time(rs.getDate("distribute_deliverman_time"));
+			expressPreOrder.setCreateTime(rs.getDate("create_time"));
+			expressPreOrder.setArrangeTime(rs.getDate("arrange_time"));
+			expressPreOrder.setDelivermanId(rs.getInt("deliverman_id"));
+			expressPreOrder.setDelivermanName(rs.getString("deliverman_name"));
+			expressPreOrder.setDistributeUserId(rs.getInt("distribute_user_id"));
+			expressPreOrder.setDistributeUserName(rs.getString("distribute_user_name"));
+			expressPreOrder.setOrderNo(rs.getString("order_no"));
+			expressPreOrder.setFeedbackFirstReasonId(rs.getInt("feedback_first_reason_id"));
+			expressPreOrder.setFeedbackFirstReason(rs.getString("feedback_first_reason"));
+			expressPreOrder.setFeedbackSecondReasonId(rs.getInt("feedback_second_reason_id"));
+			expressPreOrder.setFeedbackSecondReason(rs.getString("feedback_second_reason"));
+			expressPreOrder.setFeedbackRemark(rs.getString("feedback_remark"));
+			expressPreOrder.setFeedbackUserId(rs.getInt("feedback_user_id"));
+			expressPreOrder.setFeedbackUserName(rs.getString("feedback_user_name"));
+			expressPreOrder.setFeedbackUserName(rs.getString("feedback_time"));
+			return expressPreOrder;
+		}
+	}
+	*/
+	/**
+	 *
+	 * @description 预订单查询中用到的RowMapper
+	 * @author  刘武强
+	 * @data   2015年8月3日
+	 */
+	private final class PreOrderQueryRowMapper implements RowMapper<PreOrderQueryDateVO> {
+		@Override
+		public PreOrderQueryDateVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			PreOrderQueryDateVO preOrderQueryDateVO = new PreOrderQueryDateVO();
+			preOrderQueryDateVO.setId(rs.getInt("id"));
+			preOrderQueryDateVO.setPreOrderNo(rs.getString("pre_order_no"));
+			preOrderQueryDateVO.setExcuteState(rs.getInt("excute_state"));
+			preOrderQueryDateVO.setSendPerson(rs.getString("send_person"));
+			preOrderQueryDateVO.setCellphone(rs.getString("cellphone"));
+			preOrderQueryDateVO.setTelephone(rs.getString("telephone"));
+			preOrderQueryDateVO.setCollectAddress(rs.getString("collect_address"));
+			preOrderQueryDateVO.setBranchId(rs.getInt("branch_id"));
+			preOrderQueryDateVO.setBranchName(rs.getString("branch_name"));
+			return preOrderQueryDateVO;
+		}
+	}
+
+	public Map<String, Object> getPreOrderQueryInfo(String start, String end, String excuteType, String mobile, String preordercode, String sender, String station, long page, int pageNumber) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		List<PreOrderQueryDateVO> list = new ArrayList<PreOrderQueryDateVO>();
+		StringBuffer sql = new StringBuffer();
+		StringBuffer countsql = new StringBuffer();
+		StringBuffer where = new StringBuffer();
+		final List<Object> params = new ArrayList<Object>();
+		int count = 0;
+
+		sql.append("select * from express_ops_preorder");
+		countsql.append("select count(*) t from express_ops_preorder");
+		where.append(" ").append("where status=1");
+		if ((start != null) && StringUtils.isNotBlank(start.toString())) {
+			where.append(" ").append("and create_time>=?");
+			params.add(start);
+		}
+		if ((end != null) && StringUtils.isNotBlank(end.toString())) {
+			where.append(" ").append("and create_time<=?");
+			params.add(end);
+		}
+		if (StringUtils.isNotBlank(excuteType)) {
+			where.append(" ").append("and excute_state=?");
+			params.add(excuteType);
+		} else {
+			where.append(" ").append("and excute_state=1");
+		}
+		if (StringUtils.isNotBlank(mobile)) {
+			where.append(" ").append("and cellphone like ?");
+			params.add("%" + mobile + "%");
+		}
+		if (StringUtils.isNotBlank(preordercode)) {
+			where.append(" ").append("and pre_order_no like ?");
+			params.add("%" + preordercode + "%");
+		}
+		if (StringUtils.isNotBlank(sender)) {
+			where.append(" ").append("and send_person like ?");
+			params.add("%" + sender + "%");
+		}
+		if (StringUtils.isNotBlank(station) && !"0".equals(station)) {
+			where.append(" ").append("and branch_id=?");
+			params.add(station);
+		}
+		countsql.append(where);
+		if (page > 0) {
+			where.append(" limit " + ((page - 1) * pageNumber) + " ," + pageNumber);
+		}
+		sql.append(where);
+
+		/*PreparedStatementSetter ps = new PreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				for (int i = 0; i < params.size(); i++) {
+					ps.setObject(i + 1, params.get(i));
+				}
+			}
+		};*/
+
+		Object[] args = new Object[params.size()];
+		for (int i = 0; i < params.size(); i++) {
+			args[i] = params.get(i);
+		}
+		//查询页面数据
+		list = this.jdbcTemplate.query(sql.toString(), args, new PreOrderQueryRowMapper());
+		//查询数据总量--前面参数已经绑定，所以不需要再次绑定
+		count = this.jdbcTemplate.queryForInt(countsql.toString(), args);
+		map.put("list", list);
+		map.put("count", count);
+		return map;
+	}
 
 	private final class ExpressPreOrderRowMapper implements RowMapper<ExpressPreOrder> {
 		@Override
@@ -80,7 +229,7 @@ public class PreOrderDao {
 
 	/**
 	 * 根据条件查询预订单数量
-	 * 
+	 *
 	 * @param page
 	 * @param excuteStateList
 	 * @param delivermanId
