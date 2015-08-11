b19550e47d95ef419f9c7528fafc239d14bd0922
wangyh <wangyh@localhost>
2015-08-11 08:37:38 +0000

    【新增】包装站点单选的select【王永贺】

--- src/main/webapp/js/inputer.js
+++ src/main/webapp/js/inputer.js
@@ -13,17 +13,18 @@
         //数据支持json数据格式，可以自己扩展
     	//var tempData = "{text:'<font color=blue>请选择</font>',value:'0'},";
     	var tempData = "";
-    	var options = "";
+    	var tempText="";
+    	var tempVal="";
     	 $("#"+idkey+" option").each(function(){
-         	if($(this)[0].selected&&$(this).val()!=0){
-         		tempData = "{text:'"+$(this).text()+"',value:'"+$(this).val()+"'},";
+         	if($(this)[0].selected&&$(this).val()!=0||$(this).val()==$("#"+idkey).val()){
+         	//	tempData = "{text:'"+$(this).text()+"',value:'"+$(this).val()+"'},";
+         		tempText=$(this).text();
+         		tempVal=$(this).val();
          	}
-         	else{
-         		options += "{text:'"+$(this).text()+"',value:'"+$(this).val()+"'},";
-         	}
+         	tempData += "{text:'"+$(this).text()+"',value:'"+$(this).val()+"'},";
          });
         
-        tempData = eval("[" + tempData+options + "]");
+        tempData = eval("[" + tempData + "]");
         //数据可以用ajax到服务器去取就可以了
         $("#"+idkey).before("<input type='text' id='"+idkey+"'/>");
         $("input[id="+idkey+"]").attr('onchange',$("select[id="+idkey+"]").attr("onchange"));
@@ -31,6 +32,8 @@
         var obj = $("input[id="+idkey+"]");
 //        var obj = $("input[viewId="+idkey+"]");
         obj.inputer(idkey, tempData, 0, null);
+        $("input[id="+idkey+"]").val(tempText);
+ 		$("input[name="+idkey+"]").val(tempVal);
        
 
     }
