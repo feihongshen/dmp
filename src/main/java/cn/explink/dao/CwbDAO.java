9d1c5f307d8543b50af331d198b071303889fe34
lichongchong <lichongchong@localhost>
2015-08-11 07:51:05 +0000

    【修改】修改【分拨管理】【分拣库出库】已出库明细，导出Excel中数据重复【李冲冲】

--- src/main/java/cn/explink/dao/CwbDAO.java
+++ src/main/java/cn/explink/dao/CwbDAO.java
@@ -4873,7 +4873,7 @@ public class CwbDAO {
 
 		String sql = "SELECT cwb.* FROM express_ops_cwb_detail AS cwb LEFT JOIN express_ops_operation_time ope ON cwb.cwb = ope.cwb "
 			 	 + " WHERE ope.branchid= " + startbranchid
-				 + " AND ope.flowordertype = " + flowordertype;
+				 + " AND cwb.flowordertype = " + flowordertype;
 		if (nextbranchid > 0) {
 			sql += " AND ope.nextbranchid=" + nextbranchid;
 		}
