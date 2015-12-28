de0264fdb61240c93e9bf8112493bd5877ec6afd
song.kj <song.kj@pjbest.com>
2015-12-28 11:25:12 +0000

    【修改】修改合并代码引起的报错【宋考俊】

--- src/main/java/cn/explink/controller/addresscontroller/AddressCustomerStationMappingController.java
+++ src/main/java/cn/explink/controller/addresscontroller/AddressCustomerStationMappingController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.explink.domain.Branch;
+import cn.explink.domain.Customer;
 import cn.explink.domain.User;
 import cn.explink.domain.addressvo.AddressCustomerStationVO;
 import cn.explink.enumutil.BranchEnum;
@@ -39,22 +40,28 @@ public class AddressCustomerStationMappingController {
 
 	// 获取当前用户
 	private User getSessionUser() {
-		ExplinkUserDetail userDetail = (ExplinkUserDetail) this.securityContextHolderStrategy.getContext().getAuthentication().getPrincipal();
+		ExplinkUserDetail userDetail = (ExplinkUserDetail) this.securityContextHolderStrategy.getContext()
+				.getAuthentication().getPrincipal();
 		return userDetail.getUser();
 	}
 
 	// 进入客户站点对应关系维护界面
 	@RequestMapping("/list/{page}")
-	public String list(Model model, @PathVariable("page") long page, String customerid, String station) {
+	public String list(Model model, @PathVariable("page") long page, String customerid, String station,
+			String execute_branchid) {
 
-		List<AddressCustomerStationVO> list = this.addressCustomerStationService.getCustomerStationsArea(page, customerid, station);
+		List<AddressCustomerStationVO> list = this.addressCustomerStationService.getCustomerStationsArea(page,
+				customerid, station, execute_branchid);
 		model.addAttribute("customerid", customerid);
 		model.addAttribute("station", station);
 		model.addAttribute("listRalations", list);
+		model.addAttribute("execute_branchid", execute_branchid);
 		model.addAttribute("listCustomers", this.customerService.getPageCash());
-		model.addAttribute("listBranchs", this.getStations());
-		model.addAttribute("page_obj", new Page(this.addressCustomerStationService.getAllCount(), page, Page.ONE_PAGE_NUMBER));
+		model.addAttribute("listBranchs", this.getStation());
+		model.addAttribute("page_obj",
+				new Page(this.addressCustomerStationService.getAllCount(), page, Page.ONE_PAGE_NUMBER));
 		model.addAttribute("page", page);
+	
 		return "/address/list";
 	}
 
@@ -76,44 +83,56 @@ public class AddressCustomerStationMappingController {
 
 	// 确认添加客户站点对应关系操作
 	@RequestMapping("/create")
-	public @ResponseBody String create(HttpServletRequest request, @RequestParam("stationName") String stationName, @RequestParam("customerName") String customerName, @RequestParam("checkFlag") String checkFlag) throws Exception {
+	public @ResponseBody String create(HttpServletRequest request, @RequestParam("stations") String stationName,
+			@RequestParam("customerName") String customerName, @RequestParam("executes") String excute_branchid,
+			@RequestParam("checkFlag") String checkFlag) throws Exception {
 		User user = this.getSessionUser();
-		if(checkFlag.endsWith("0")){
-			//校验是否添加相同数据
-			int checkF = this.addressCustomerStationService.checkSame(customerName,stationName);
-			if(checkF==1){
+		if (checkFlag.endsWith("0")) {
+			// 校验是否添加相同数据
+			int checkF = this.addressCustomerStationService.checkSame(customerName, stationName);
+			if (checkF == 1) {
 				return "{\"errorCode\":1,\"error\":\"相同的客户、区域已存在该站点，系统将平均分配执行站点，是否继续创建？\"}";
-			}else if(checkF==2){
+			} else if (checkF == 2) {
 				return "{\"errorCode\":2,\"error\":\"相同客户存在相同站点，创建失败\"}";
 			}
 		}
-		this.addressCustomerStationService.create(customerName, stationName, user);
+		this.addressCustomerStationService.create(customerName, stationName, user, excute_branchid);
 		return "{\"errorCode\":0,\"error\":\"创建成功\"}";
 	}
 
 	// 进入编辑页
 	@RequestMapping("/edit/{id}")
 	public String edit(Model model, @PathVariable("id") long id) {
-		model.addAttribute("addressCustomerStationVO", this.addressCustomerStationService.getCustomerStationByid(id));
+		AddressCustomerStationVO  addressCustomerStationVO= this.addressCustomerStationService.getCustomerStationByid(id);
+		List<Branch> branchList=  this.getStations();
+		String[] executes = addressCustomerStationVO.getBranchid().split(",");
+		
+		model.addAttribute("addressCustomerStationVO",addressCustomerStationVO );
 		model.addAttribute("listCustomers", this.customerService.getPageCash());
+		
+		
+		
+		
 		model.addAttribute("listBranchs", this.getStations());
 		return "/address/edit";
 	}
 
 	// 确认修改客户站点对应关系操作
 	@RequestMapping("/save")
-	public @ResponseBody String save(Model model, @RequestParam("customerName") String customerName, @RequestParam("branchName") String branchName,@RequestParam("checkFlag") String checkFlag, @RequestParam("customerId") String customerId) throws Exception {
+	public @ResponseBody String save(Model model, @RequestParam("customerName") String customerName,
+			@RequestParam("stations") String branchName, @RequestParam("checkFlag") String checkFlag,@RequestParam("executes") String excute_branchid,
+			@RequestParam("id") String id) throws Exception {
 		User user = this.getSessionUser();
-		if(checkFlag.endsWith("0")){
-			//校验是否添加相同数据
-			int checkF = this.addressCustomerStationService.checkSame(customerId,branchName);
-			if(checkF==1){
+		if (checkFlag.endsWith("0")) {
+			// 校验是否添加相同数据
+			int checkF = this.addressCustomerStationService.checkSame(id, branchName);
+			if (checkF == 1) {
 				return "{\"errorCode\":1,\"error\":\"相同的客户、区域已存在该站点，系统将平均分配执行站点，是否继续修改？\"}";
-			}else if(checkF==2){
+			} else if (checkF == 2) {
 				return "{\"errorCode\":0,\"error\":\"相同客户存在相同站点，保存失败\"}";
 			}
 		}
-		this.addressCustomerStationService.updateById(Long.parseLong(customerName), branchName, user);
+		this.addressCustomerStationService.updateById(Long.parseLong(id),branchName,customerName,  user, excute_branchid);
 		return "{\"errorCode\":0,\"error\":\"保存成功\"}";
 	}
 
@@ -128,11 +147,18 @@ public class AddressCustomerStationMappingController {
 		List<Branch> list = this.branchService.getPageCash();
 		List<Branch> listStation = new ArrayList<Branch>();
 		for (Branch branch : list) {
-			if (branch.getSitetype() == BranchEnum.ZhanDian.getValue()&&"1".equals(branch.getBrancheffectflag())) {
+			if (branch.getSitetype() == BranchEnum.ZhanDian.getValue() && "1".equals(branch.getBrancheffectflag())) {
 				listStation.add(branch);
 			}
 		}
 		return listStation;
 	}
-
+	public List<Branch> getStation() {
+		List<Branch> list = this.branchService.getPageCashs();
+	
+		
+		
+		
+		return list;
+	}
 }
