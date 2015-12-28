bdedee8f8223c8494b9bc8b5d173a3b55b695eaa
mali <mali@pjbest.com>
2015-12-28 11:14:15 +0000

    【修改】修改合并代码产生的问题【马力】

--- src/main/java/cn/explink/service/addressmatch/AddressCustomerStationService.java
+++ src/main/java/cn/explink/service/addressmatch/AddressCustomerStationService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import cn.explink.dao.AddressCustomerStationDao;
 import cn.explink.dao.BranchDAO;
@@ -44,35 +45,30 @@ public class AddressCustomerStationService {
 		HashMap<String, List<AddressCustomerStationVO>> map = new HashMap<String, List<AddressCustomerStationVO>>();
 		List<AddressCustomerStationVO> list = this.addressCustomerStationDao.getCustomerStationsPage();
 		for (AddressCustomerStationVO addressCustomerStationVO : list) {
-			map.put("" + addressCustomerStationVO.getCustomerName(), this.addressCustomerStationDao.getCustomerStationByCustomerId(addressCustomerStationVO.getCustomerid()));
+			map.put("" + addressCustomerStationVO.getCustomerName(), this.addressCustomerStationDao
+					.getCustomerStationByCustomerId(addressCustomerStationVO.getCustomerid()));
 		}
 		return map;
 	}
 
 	// 获取页面所需要的list(一个客户匹配一个站点用到)带分页
-	public List<AddressCustomerStationVO> getCustomerStationsArea(Long page, String customerid, String station) {
-		List<AddressCustomerStationVO> list = this.addressCustomerStationDao.getAllCustomerStationsByPage(page, customerid, station);
-		List<Branch> listBranchs = this.branchDAO.getAllBranches();
-		for (AddressCustomerStationVO addressCustomerStationVO : list) {
-			for (Branch branch : listBranchs) {
-				if (addressCustomerStationVO.getBranchid() == branch.getBranchid()) {
-					String area = branch.getBranchprovince().length()==0?"":(branch.getBranchprovince() + "-") + (branch.getBranchcity().length()==0?"":(branch.getBranchcity() + "-")) + (branch.getBrancharea().length()==0?"":branch.getBrancharea() + "-") +branch.getBranchstreet();
-					if(area.endsWith("-")){
-						area = area.substring(0,area.length()-1);
-					}
-					addressCustomerStationVO.setArea(area);
-				}
-			}
-		}
+	public List<AddressCustomerStationVO> getCustomerStationsArea(Long page, String customerid, String station,
+			String execute_branchid) {
+		List<AddressCustomerStationVO> list = this.addressCustomerStationDao.getAllCustomerStationsByPage(page,
+				customerid, station, execute_branchid);
 		return list;
 	}
 
 	// 根据branchId获取站点区域
 	public String getAreaByBranchId(Long branchid) {
 		Branch branch = this.branchDAO.getBranchByBranchid(branchid);
-		String area = branch.getBranchprovince().length()==0?"":(branch.getBranchprovince() + "-") + (branch.getBranchcity().length()==0?"":(branch.getBranchcity() + "-")) + (branch.getBrancharea().length()==0?"":branch.getBrancharea() + "-") +branch.getBranchstreet();
-		if(area.endsWith("-")){
-			area = area.substring(0,area.length()-1);
+		String area = branch.getBranchprovince().length() == 0 ? ""
+				: (branch.getBranchprovince() + "-")
+						+ (branch.getBranchcity().length() == 0 ? "" : (branch.getBranchcity() + "-"))
+						+ (branch.getBrancharea().length() == 0 ? "" : branch.getBrancharea() + "-")
+						+ branch.getBranchstreet();
+		if (area.endsWith("-")) {
+			area = area.substring(0, area.length() - 1);
 		}
 		return area;
 	}
@@ -87,52 +83,58 @@ public class AddressCustomerStationService {
 		return this.addressCustomerStationDao.getCustomerStationByCustomerid(customerid);
 	}
 
-	//判断是否添加了相同的数据
-	public int checkSame(String customerid,String stationid){
-		List<AddressCustomerStationVO> list = this.addressCustomerStationDao.getCustomerStationByCustomerid(Long.parseLong(customerid));
-		String areaSrc = getAreaByBranchId(Long.parseLong(stationid));
+	// 判断是否添加了相同的数据
+	public int checkSame(String customerid, String stationName) {
+		List<AddressCustomerStationVO> list = this.addressCustomerStationDao
+				.getCustomerStationByCustomerid(Long.parseLong(customerid));
+//		String areaSrc = getAreaByBranchId(Long.parseLong(stationName));
 		int a = 0;
-		for(AddressCustomerStationVO addressCustomerStationVO : list){
-			String areaTar = getAreaByBranchId(addressCustomerStationVO.getBranchid().longValue());
-			//根据站点判断
-			if(addressCustomerStationVO.getBranchid()==Integer.parseInt(stationid)){
-				return 2;
-			}
-			//根据区域判断
-			if(areaSrc.length()>0&&areaSrc.equals(areaTar)){
-				a = 1;
-			}
+		for (AddressCustomerStationVO addressCustomerStationVO : list) {
+			/*
+			 * String areaTar =
+			 * getAreaByBranchId(addressCustomerStationVO.getBranchid().
+			 * longValue()); // 根据站点判断 if
+			 * (addressCustomerStationVO.getBranchid() ==
+			 * Integer.parseInt(stationid)) { return 2; } // 根据区域判断 if
+			 * (areaSrc.length() > 0 && areaSrc.equals(areaTar)) { a = 1; }
+			 */
 		}
 		return a;
 	}
-	
-	// 根据customerid创建多条记录
-	public void create(String customerName, String stationName, User user) {
-		String[] stationNames = stationName.split(",");
-		int length = stationNames.length;
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-		for (int a = 0; a < length; a++) {
 
-			this.addressCustomerStationDao.create(customerName, stationNames[a], user.getUserid(), user.getUsername(), df.format(new Date()));
-		}
+	// 根据customerid创建多条记录
+	public void create(String customerName, String stationName, User user, String excute_branckid) {
+		/*
+		 * String[] stationNames = stationName.split(","); int length =
+		 * stationNames.length;
+		 */
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
+		// for (int a = 0; a < length; a++) {
+
+		this.addressCustomerStationDao.create(customerName, stationName, excute_branckid, user.getUserid(),
+				user.getUsername(), df.format(new Date()));
+		// }
 	}
 
 	// 根据客户id修改站点信息
-	public void updateByCustomerId(Long customerid, String stationName, User user) {
+	public void updateByCustomerId(Long customerid, String stationName, User user ,String excute_branckid) {
 		// 根据客户id删掉之前的数据
 		this.addressCustomerStationDao.delByCustomerId(customerid);
 		// 根据所选择的客户的站点，将所有站点都添加到该客户下
-		this.create(customerid.toString(), stationName, user);
+		this.create(customerid.toString(), stationName, user,excute_branckid);
 	}
-	
-	//根据id修改站点信息
-	public void updateById(Long id, String stationName, User user){
+
+	// 根据id修改站点信息
+	public void updateById(Long id, String stationName,String customerName, User user, String excute_branchid) {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 设置日期格式
-		this.addressCustomerStationDao.updateById(id,stationName,user.getUserid(),user.getUsername(),df.format(new Date()));
+		Integer customerid=Integer.parseInt(customerName);
+		this.addressCustomerStationDao.updateById(id, stationName, customerid,excute_branchid,user.getUserid(), user.getUsername(),
+				df.format(new Date()));
 	}
 
 	// 根据id删除记录
 	public void delById(Long id) {
 		this.addressCustomerStationDao.delById(id);
 	}
+	
 }
