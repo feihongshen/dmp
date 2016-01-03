d062d34bd6ee3a3ff48a34cbd2f0be4ff8e9da76
barry.liang <barry.liang@pjbest.com>
2016-01-03 11:23:35 +0000

    【修改】优化运单状态接口【梁宇荣】

--- src/main/java/cn/explink/b2c/tps/TPSCarrierOrderStatusTimmer.java
+++ src/main/java/cn/explink/b2c/tps/TPSCarrierOrderStatusTimmer.java
@@ -108,16 +108,16 @@ public class TPSCarrierOrderStatusTimmer {
 				long msgid= this.autoExceptionService.createAutoExceptionMsg(JSON.toJSON(response).toString(), AutoInterfaceEnum.insertTransportNo.getValue());
 				this.autoExceptionService.createAutoExceptionDetail(cwb, transcwb, "无订单号或者不存在运单号", AutoExceptionStatusEnum.xinjian.getValue(), msgid, 0);
 				logger.error("tps返回运单状态数据中无订单号或者不存在运单号,返回数据："+JSON.toJSON(response).toString());
-				continue;
-			}
-			try {
-				// 录入运单号
-				tPSCarrierOrderStatusTimmer.boundTranscwb(response);
-			} catch (Exception e) {
-				//保存录入运单号时出现异常的运单状态数据
-				long msgid= this.autoExceptionService.createAutoExceptionMsg(JSON.toJSON(response).toString(), AutoInterfaceEnum.insertTransportNo.getValue());
-				this.autoExceptionService.createAutoExceptionDetail(cwb, transcwb,e.getMessage(), AutoExceptionStatusEnum.xinjian.getValue(), msgid, 0);
-				logger.error("录入运单号异常:"+e.getMessage()+" 返回运单状态数据:"+JSON.toJSON(response).toString());
+			}else{
+				try {
+					// 录入运单号
+					tPSCarrierOrderStatusTimmer.boundTranscwb(response);
+				} catch (Exception e) {
+					//保存录入运单号时出现异常的运单状态数据
+					long msgid= this.autoExceptionService.createAutoExceptionMsg(JSON.toJSON(response).toString(), AutoInterfaceEnum.insertTransportNo.getValue());
+					this.autoExceptionService.createAutoExceptionDetail(cwb, transcwb,e.getMessage(), AutoExceptionStatusEnum.xinjian.getValue(), msgid, 0);
+					logger.error("录入运单号异常:"+e.getMessage()+" 返回运单状态数据:"+JSON.toJSON(response).toString());
+				}
 			}
 			
 			//更新seq
