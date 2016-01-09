6aa9a87d1e630bf8095f564dd534edee0099364f
shenhongfei <shenhongfei@localhost>
2016-01-09 03:02:47 +0000

    【修改】订单状态模块【shenhf】

--- src/main/java/cn/explink/enumutil/CwbStateEnum.java
+++ src/main/java/cn/explink/enumutil/CwbStateEnum.java
@@ -6,8 +6,8 @@ import java.util.Map;
 import cn.explink.exception.ExplinkException;
 
 public enum CwbStateEnum {
-	WUShuju(0, "无数据"), PeiShong(1, "配送"), TuiHuo(2, "退货"), DiuShi(3, "丢失"), WuXiaoShuJu(4, "无效数据"), TuiGongYingShang(5, "退供货商"), ZhongZhuan(6, "中转")
-	,OXO_JIT(7,"OXO_JIT");
+	WUShuju(0, "无数据"), PeiShong(1, "配送"), TuiHuo(2, "退货"), DiuShi(3, "完全丢失"), WuXiaoShuJu(4, "无效数据"), TuiGongYingShang(5, "退供货商"), ZhongZhuan(6, "中转")
+	,OXO_JIT(7,"OXO_JIT"),BUFENDIUSHI(8,"部分丢失"),WANQUANPOSUN(9,"完全破损"),BUFENPOSUN(10,"部分破损") ;
 
 	private int value;
 	private String text;
