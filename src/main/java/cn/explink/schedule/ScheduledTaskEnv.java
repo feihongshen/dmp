eabb86d18a2d6b3411ad218b56b5473988d9eb65
pengfei.wan <pengfei.wan@vipshop.com>
2016-02-19 10:43:59 +0800

    对定时器任务，【单机任务】增加redis记录，用于排他执行。 待完善或使用 redisson 分布式锁。。。

--- src/main/java/cn/explink/schedule/ScheduledTaskEnv.java
+++ src/main/java/cn/explink/schedule/ScheduledTaskEnv.java
@@ -1,14 +1,63 @@
 package cn.explink.schedule;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashSet;
 import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 
 /**
- * 任务调度运行环境
+ * 任务调度运行环境，引入CacheManager改造成分布式缓存，后续待完善。
  */
 public class ScheduledTaskEnv {
 
 	private static ScheduledTaskEnv instance = new ScheduledTaskEnv();
+
+	@Autowired
+	private CacheManager cacheManager = null;
+
+	private static final String cacheName = "scheduledTaskEnv";
+
+	private static String macAddress = null;
+
+	private String lookup_mac() {
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			System.out.println("[ScheduledTaskEnv] Current IP address : "
+					+ ip.getHostAddress());
+			NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+			byte[] mac = network.getHardwareAddress();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < mac.length; i++) {
+				sb.append(String.format("%02X%s", mac[i],
+						(i < mac.length - 1) ? "-" : ""));
+			}
+			System.out.println("[ScheduledTaskEnv] Current MAC address : "
+					+ sb.toString());
+			if (sb.length() > 0) {
+				return sb.toString();
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private synchronized Cache getCache() {
+		if (macAddress == null && (macAddress = lookup_mac()) == null) {
+			return null;
+		}
+		if (cacheManager != null) {
+			return cacheManager.getCache(cacheName);
+		}
+		return null;
+	}
 
 	/**
 	 * 已经提交到线程池的任务id
@@ -23,14 +72,31 @@ public class ScheduledTaskEnv {
 	}
 
 	public void addTask(Long taskId) {
+		Cache cache = getCache();
+		if (cache != null) {
+			cache.put(taskId, macAddress);
+		}
 		taskIds.add(taskId);
 	}
 
 	public void removeTask(Long taskId) {
+		Cache cache = getCache();
+		if (cache != null) {
+			cache.evict(taskId);
+		}
 		taskIds.remove(taskId);
 	}
 
 	public boolean hasTask(Long taskId) {
+		Cache cache = getCache();
+		if (cache != null) {
+			if (cache.get(taskId) != null) {
+				String value = (String) cache.get(taskId).get();
+				if (value != null && value.compareTo(macAddress) != 0) {
+					return true;
+				}
+			}
+		}
 		return taskIds.contains(taskId);
 	}
 
