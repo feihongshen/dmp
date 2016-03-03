216634129d6b8a855307adeecee6d1c8e3cb15f7
pengfei.wan <pengfei.wan@vipshop.com>
2016-03-03 09:41:10 +0800

    改造本地 Map 为 RedisMap

--- src/main/java/cn/explink/b2c/yihaodian/YihaodianController.java
+++ src/main/java/cn/explink/b2c/yihaodian/YihaodianController.java
@@ -17,6 +17,8 @@ import cn.explink.b2c.tools.JiontDAO;
 import cn.explink.b2c.tools.JointService;
 import cn.explink.dao.BranchDAO;
 import cn.explink.enumutil.BranchEnum;
+import cn.explink.util.RedisMap;
+import cn.explink.util.impl.RedisMapImpl;
 
 @Controller
 @RequestMapping("/yihaodian")
@@ -32,9 +34,9 @@ public class YihaodianController {
 	@Autowired
 	YihaodianInsertCwbDetailTimmer timmer;
 
-	public static Map<String, Integer> threadMap;
-	static { // 静态初始化 以下变量,用于判断线程是否在执行
-		threadMap = new HashMap<String, Integer>();
+	public static RedisMap<String, Integer> threadMap;
+	static { // 静态初始化 以下变量,用于判断线程是否在执行		
+		threadMap = new RedisMapImpl<String, Integer>("YihaodianController");
 		threadMap.put("yihaodian_hander", 0);
 	}
 
