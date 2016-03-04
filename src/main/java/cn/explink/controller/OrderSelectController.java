f9fc750198089dfd241fe91c604712f27698a261
WangHao <hao15.wang@pjbest.com>
2016-03-04 11:02:49 +0800

    客服管理-订单查询：在揽件录入和揽件出站时，加入操作员联系电话。

--- src/main/java/cn/explink/controller/OrderSelectController.java
+++ src/main/java/cn/explink/controller/OrderSelectController.java
@@ -1401,11 +1401,11 @@ public class OrderSelectController {
 						comment);
 			}
 			if (orderFlowAll.getFlowordertype() == FlowOrderTypeEnum.LanJianRuZhan.getValue()) {
-				return MessageFormat.format("货物被<font color =\"red\">[{0}]</font>揽件入站；备注：<font color =\"red\">[{1}]</font>", this.userDAO.getUserByUserid(orderFlowAll.getUserid()).getRealname(),
+				return MessageFormat.format("货物被<font color =\"red\">[{0}]</font>揽件入站；联系电话：<font color =\"red\">[{1}]</font>; 备注：<font color =\"red\">[{2}]</font>", this.userDAO.getUserByUserid(orderFlowAll.getUserid()).getRealname(),phone,
 						comment);
 			}
 			if (orderFlowAll.getFlowordertype() == FlowOrderTypeEnum.LanJianChuZhan.getValue()) {
-				return MessageFormat.format("货物被<font color =\"red\">[{0}]</font>揽件出站；备注：<font color =\"red\">[{1}]</font>", this.userDAO.getUserByUserid(orderFlowAll.getUserid()).getRealname(),
+				return MessageFormat.format("货物被<font color =\"red\">[{0}]</font>揽件出站；联系电话：<font color =\"red\">[{1}]</font>; 备注：<font color =\"red\">[{2}]</font>", this.userDAO.getUserByUserid(orderFlowAll.getUserid()).getRealname(),phone,
 						comment);
 			}
 			// ============================add
