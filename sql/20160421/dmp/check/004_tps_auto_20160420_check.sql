4523012f93983cbf31a51bc5cdf6ec3760e4829b
warrior.wang <warrior.wang@vipshop.com>
2016-04-21 11:02:29 +0800

    整理20160421的check/rollback sql

---
+++ sql/20160421/dmp/check/004_tps_auto_20160420_check.sql
@@ -0,0 +1,4 @@
+select count(operatetype) from express_auto_exception_detail;
+select count(transportno) from express_auto_order_status_tmp;
+select count(cargovolume) from express_ops_transcwb_detail;
+select count(carrealweight) from express_ops_transcwb_detail;
