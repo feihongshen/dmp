701414a780aace6eed63b56d55e712fb93a20901
jeef.fang <jeef.fang@vipshop.com>
2016-04-27 14:48:33 +0800

    plcs #none 修改菜单脚本

--- sql/20160421/dmp/001_redis_20160412.sql
+++ sql/20160421/dmp/001_redis_20160412.sql
@@ -1,7 +1,22 @@
--- 增加redis查询菜单
-INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('3015', '1', 'DMP-REDIS缓存', '3015', 'redis/list/?', 
+-- 增加redis查询菜单(二级菜单)
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('3050', '1', 'REDIS缓存', '3050', '', 
 (select ID from dmp40_function a where a.functionname='系统设置' limit 1));
-INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('3016', '1', 'OMS-REDIS缓存', '3016', '${omsUrl}redis/list/?', 
+-- 增加redis查询菜单(三级菜单)
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('305010', '2', 'DMP缓存', '305010', 'redis/list/?', 
+(select ID from dmp40_function a where a.functionname='REDIS缓存' limit 1));
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('305011', '2', 'OMS缓存', '305011', '${omsUrl}redis/list/?', 
+(select ID from dmp40_function a where a.functionname='REDIS缓存' limit 1));
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('305012', '2', 'EAP缓存', '305012', '${eapUrl}redis/list.do?', 
+(select ID from dmp40_function a where a.functionname='REDIS缓存' limit 1));
+
+
+-- 删除原MQ异常菜单
+delete from dmp40_function where id in (3011,3013);
+-- 增加MQ异常(二级菜单)
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('3060', '1', 'MQ异常', '3060', '', 
 (select ID from dmp40_function a where a.functionname='系统设置' limit 1));
-INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('3017', '1', 'EAP-REDIS缓存', '3017', '${eapUrl}redis/list.do?', 
-(select ID from dmp40_function a where a.functionname='系统设置' limit 1));
+-- 增加MQ异常(三级菜单)
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('306010', '2', 'DMP异常', '306010', 'mqexception/list/1?', 
+(select ID from dmp40_function a where a.functionname='MQ异常' limit 1));
+INSERT INTO `dmp40_function` (`ID`,`functionlevel`, `functionname`, `functionorder`, `functionurl`, `parentfunctionid`) VALUES ('306011', '2', 'OMS异常', '306011', '${omsUrl}mqexception/list/1?', 
+(select ID from dmp40_function a where a.functionname='MQ异常' limit 1));
