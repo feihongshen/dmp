b6d7709c7d603f6bb2a2edb0582b25449a1fbc60
warrior.wang <warrior.wang@vipshop.com>
2016-04-27 17:35:14 +0800

    整理SQl，以便申请变更

--- sql/20160421/dmp/check/002_update_time_20160418_check.sql
+++ sql/20160421/dmp/check/002_update_time_20160418_check.sql
@@ -1,10 +1,9 @@
--- 增加时间戳字段
-ALTER TABLE `express_set_branch` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `express_set_customer_info` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `express_set_user` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `express_ops_order_intowarhouse` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `express_set_reason` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `express_ops_orderback_record` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `fn_customer_bill` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `express_set_role_new` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
-ALTER TABLE `ops_ypdjhandlerecord` ADD COLUMN `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间-MySQL维护';
+select update_time from `express_set_branch` where 1 = 2;	-- 没有报错
+select update_time from `express_set_customer_info` where 1 = 2;	-- 没有报错
+select update_time from `express_set_user` where 1 = 2;	-- 没有报错
+select update_time from `express_ops_order_intowarhouse` where 1 = 2;	-- 没有报错
+select update_time from `express_set_reason` where 1 = 2;	-- 没有报错
+select update_time from `express_ops_orderback_record` where 1 = 2;	-- 没有报错
+select update_time from `fn_customer_bill` where 1 = 2;	-- 没有报错
+select update_time from `express_set_role_new` where 1 = 2;	-- 没有报错
+select update_time from `ops_ypdjhandlerecord` where 1 = 2;	-- 没有报错
