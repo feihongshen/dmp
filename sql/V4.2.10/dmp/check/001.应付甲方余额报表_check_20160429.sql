635f068c38d2a45047ca7791f51b3ea3e87e3b5d
warrior.wang <warrior.wang@vipshop.com>
2016-04-28 19:13:00 +0800

    整理规范sql

--- sql/V4.2.10/dmp/check/001.应付甲方余额报表_check_20160429.sql
+++ sql/V4.2.10/dmp/check/001.应付甲方余额报表_check_20160429.sql
@@ -1,14 +1,15 @@
-select * from `fn_brance_report_detail` where 1 = 2;	-- 没有报错
-
-select * from `fn_brance_report_adjust` where 1 = 2;	-- 没有报错
- 
-select * from `fn_brance_report` where 1 = 2;	-- 没有报错
- 
-select * from `fn_cust_pay_remit_detail` where 1 = 2;	-- 没有报错
- 
-select * from `fn_cust_pay_report_cfg` where 1 = 2;	-- 没有报错 
- 
-
-select * from `dmp40_function` where `ID` =  '802060' and `functionname`= '客户结算信息管理';	-- 有一条结果
-select * from `dmp40_function` where `ID` =  '802061' and `functionname`= '实付客户记录管理';	-- 有一条结果
-select * from `dmp40_function` where `ID` =  '802062' and `functionname`= '应付甲方余额报表';	-- 有一条结果
+SELECT '新建应付甲方报表明细表结构' AS '脚本文件名',IF((SELECT COUNT(1) FROM information_schema.tables WHERE table_name = 'fn_brance_report_detail') > 0,'success','failed') AS '执行结果'
+UNION ALL
+SELECT '新建应付调整单明细表' AS '脚本文件名',IF((SELECT COUNT(1) FROM information_schema.tables WHERE table_name = 'fn_brance_report_adjust') > 0,'success','failed') AS '执行结果'
+UNION ALL
+SELECT '应付甲方报表的表结构' AS '脚本文件名',IF((SELECT COUNT(1) FROM information_schema.tables WHERE table_name = 'fn_brance_report') > 0,'success','failed') AS '执行结果'
+UNION ALL
+SELECT '应付甲方报表汇款信息' AS '脚本文件名',IF((SELECT COUNT(1) FROM information_schema.tables WHERE table_name = 'fn_cust_pay_remit_detail') > 0,'success','failed') AS '执行结果'
+UNION ALL
+SELECT '应付甲方报表客户信息' AS '脚本文件名',IF((SELECT COUNT(1) FROM information_schema.tables WHERE table_name = 'fn_cust_pay_report_cfg') > 0,'success','failed') AS '执行结果'
+UNION ALL
+SELECT '应付甲方余额报表菜单' AS '脚本文件名',IF((
+(SELECT COUNT(1) FROM dmp40_function WHERE ID = '802060' AND functionname='客户结算信息管理')+
+(SELECT COUNT(1) FROM dmp40_function WHERE ID = '802061' AND functionname='实付客户记录管理')+
+(SELECT COUNT(1) FROM dmp40_function WHERE ID = '802062' AND functionname='应付甲方余额报表')
+)= 3 ,'success','failed') AS '执行结果'
