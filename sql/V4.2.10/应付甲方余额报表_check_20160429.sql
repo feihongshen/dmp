19b83cc36b5b6c88ff1ac31708892bd1bc785e4c
jeef.fang <jeef.fang@vipshop.com>
2016-04-28 20:04:26 +0800

    plcs #none 合并脚本中，格式问题2

--- sql/V4.2.10/应付甲方余额报表_check_20160429.sql
+++ sql/V4.2.10/应付甲方余额报表_check_20160429.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 select * from `fn_brance_report_detail` where 1 = 2;	-- 没有报错
 
 select * from `fn_brance_report_adjust` where 1 = 2;	-- 没有报错
@@ -11,4 +12,19 @@ select * from `fn_cust_pay_report_cfg` where 1 = 2;	-- 没有报错
 
 select * from `dmp40_function` where `ID` =  '802060' and `functionname`= '客户结算信息管理';	-- 有一条结果
 select * from `dmp40_function` where `ID` =  '802061' and `functionname`= '实付客户记录管理';	-- 有一条结果
+=======
+select * from `fn_brance_report_detail` where 1 = 2;	-- 没有报错
+
+select * from `fn_brance_report_adjust` where 1 = 2;	-- 没有报错
+ 
+select * from `fn_brance_report` where 1 = 2;	-- 没有报错
+ 
+select * from `fn_cust_pay_remit_detail` where 1 = 2;	-- 没有报错
+ 
+select * from `fn_cust_pay_report_cfg` where 1 = 2;	-- 没有报错 
+ 
+
+select * from `dmp40_function` where `ID` =  '802060' and `functionname`= '客户结算信息管理';	-- 有一条结果
+select * from `dmp40_function` where `ID` =  '802061' and `functionname`= '实付客户记录管理';	-- 有一条结果
+>>>>>>> c36950a... 添加应付甲方余额报表sql【上官镜文】
 select * from `dmp40_function` where `ID` =  '802062' and `functionname`= '应付甲方余额报表';	-- 有一条结果
