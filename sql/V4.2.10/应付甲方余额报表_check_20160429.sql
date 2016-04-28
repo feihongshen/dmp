66f632aa352fdc2c77748d3c5af3b942377f03aa
bruce.shangguan <bruce.shangguan@pjbest.com>
2016-04-28 17:57:46 +0800

    添加应付甲方余额报表sql【上官镜文】

--- sql/V4.2.10/应付甲方余额报表_check_20160429.sql
+++ sql/V4.2.10/应付甲方余额报表_check_20160429.sql
@@ -6,7 +6,7 @@ select * from `fn_brance_report` where 1 = 2;	-- 没有报错
  
 select * from `fn_cust_pay_remit_detail` where 1 = 2;	-- 没有报错
  
-select * from `fn_cust_pay_report_cfg` where 1 = 2;	-- 没有报错
+select * from `fn_cust_pay_report_cfg` where 1 = 2;	-- 没有报错       
  
 
 select * from `dmp40_function` where `ID` =  '802060' and `functionname`= '客户结算信息管理';	-- 有一条结果
