be2cd1bfeee7d6420f134ba64a2ac812a046bdc6
neo01.huang <neo01.huang@pjbest.com>
2016-05-14 19:48:27 +0800

    加入reserveOrderQuery.js

---
+++ src/main/webapp/js/express2/reserveOrder/reserveOrderQuery.js
@@ -0,0 +1,10 @@
+
+/**
+ * 预约单号格式化
+ * @param {Object} value the field value.
+ * @param {Object} row the row record data.
+ * @param {Object} index the row index.
+ */
+function reserveOrderNoFormatter(value, row, index) {
+	return '<a href="#">'+ value +</a>';
+}
