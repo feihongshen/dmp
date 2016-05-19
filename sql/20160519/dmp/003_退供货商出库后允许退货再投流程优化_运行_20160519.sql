dc9648f4c2b987dc2cb10e29bf13317deb9e389b
chunlei05.li <chunlei05.li@pjbest.com>
2016-05-19 14:10:08 +0800

    退供货商出库后允许退货再投流程优化sql

---
+++ sql/20160519/dmp/003_退供货商出库后允许退货再投流程优化_运行_20160519.sql
@@ -0,0 +1,3 @@
+insert ignore express_set_cwb_state_control(fromstate, tostate) VALUES (27, 15);
+insert ignore express_set_cwb_allstate_control(cwbstate, toflowtype) VALUES (5, 15);
+insert ignore express_set_transcwb_allstate_control(tanscwbstate, toflowtype) VALUES (5, 15);
