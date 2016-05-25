c0ae99029b62499d88ae8f9d6aad2f13d51b411f
jian.xie <jian.xie@vipshop.com>
2016-05-25 10:37:20 +0800

    【脚本】快递二期

---
+++ sql/20160602/004_临时表alter_table_express_ops_cwb_exprss_detail_temp_20160523.sql
@@ -0,0 +1,9 @@
+alter table express_ops_cwb_exprss_detail_temp 
+add column cnor_corp_no varchar(100) NOT NULL DEFAULT '' COMMENT '寄件单位编码',
+add column cnor_corp_name varchar(100) NOT NULL DEFAULT '' COMMENT '寄件单位名称', 
+add column freight decimal(18,2) NOT NULL DEFAULT '0.00' COMMENT '应收运费',
+add column account_id varchar(100) NOT NULL DEFAULT '' COMMENT '月结账号',
+add column packing_fee decimal(18,2) NOT NULL DEFAULT '0.00' COMMENT '包装费',
+add column express_image varchar(100) NOT NULL DEFAULT '' COMMENT '图片url',
+add column cnee_corp_name varchar(50) NOT NULL DEFAULT '' COMMENT '收货产品类型',
+add column is_accept_prov int not null DEFAULT '0' COMMENT '0、派件省，1、揽件省';
