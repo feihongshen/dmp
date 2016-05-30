2663eb9dce6befb5c6e969183c934ef0828ed8d5
jian.xie <jian.xie@vipshop.com>
2016-05-30 21:10:13 +0800

    删除express_ops_cwb_detail多余字段

--- sql/20160602/check/003_主表express_ops_cwb_detail添加字段_check.sql
+++ sql/20160602/check/003_主表express_ops_cwb_detail添加字段_check.sql
@@ -1,7 +1,6 @@
 desc express_ops_cwb_detail 'cnor_corp_no';
 desc express_ops_cwb_detail 'cnor_corp_name'; 
 desc express_ops_cwb_detail 'account_id';
-desc express_ops_cwb_detail 'packing_fee';
 desc express_ops_cwb_detail 'express_image';
 desc express_ops_cwb_detail 'cnee_corp_name';
 desc express_ops_cwb_detail 'express_product_type';
--- sql/20160602/rollback/003_主表express_ops_cwb_detail添加字段_rollback.sql
+++ sql/20160602/rollback/003_主表express_ops_cwb_detail添加字段_rollback.sql
@@ -2,7 +2,6 @@ alter table express_ops_cwb_detail
 DROP column cnor_corp_no ,
 DROP column cnor_corp_name , 
 DROP column account_id ,
-DROP column packing_fee ,
 DROP column express_image ,
 DROP column cnee_corp_name ,
 DROP column express_product_type ;
