1fed2f36dd8488e221d959e61ad7c5d84ae42cd7
steve01.peng <steve01.peng@dongjie-peng.pjbest.com>
2016-05-31 20:14:28 +0800

    [修改] 修改一下杨建提出需要在6月12日上线的需求 1。 去掉“分拨管理-站点操作-快递揽件录入”界面， 2. 修改“订单处理-揽收运单补录”为：“订单处理-快递揽件录入”

---
+++ sql/20160612/dmp/001_删除快递揽件录入页面.sql
@@ -0,0 +1,3 @@
+DELETE FROM `dmp40_function` WHERE `ID` = '504088'  AND  `functionname` = '快递揽件录入';
+
+UPDATE `dmp40_function` SET `functionname` = '快递揽件录入' WHERE `ID` = '4010'  AND  `functionname` = '揽收运单补录';
---
+++ sql/20160612/dmp/check/001_删除快递揽件录入页面_check.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `dmp40_function` WHERE `ID` = '504088'  AND  `functionname` = '快递揽件录入';	-- 没有记录
+
+SELECT count(*) FROM `dmp40_function` WHERE `ID` = '4010'  AND  `functionname` = '快递揽件录入'; -- 返回1
