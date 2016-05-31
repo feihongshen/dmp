16d87eabdaa58c7ab600357b2046c1197328bfe2
chunlei05.li <chunlei05.li@pjbest.com>
2016-05-31 20:25:39 +0800

    面单打印页面样式调整

--- src/main/java/cn/explink/dao/CwbDAO.java
+++ src/main/java/cn/explink/dao/CwbDAO.java
@@ -359,7 +359,7 @@ public class CwbDAO {
 			cwbOrder.setMpsallarrivedflag(rs.getInt("mpsallarrivedflag"));
 			cwbOrder.setInstationname(rs.getString("instationname"));
 			cwbOrder.setMonthsettleno(rs.getString("monthsettleno"));
-			
+			cwbOrder.setAnnouncedvalue(rs.getBigDecimal("announcedvalue"));
 			if(isFilterUserInfo) {
 				CwbDAO.this.setValueByUser(rs, cwbOrder);
 			} else {
