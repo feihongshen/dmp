687a145ee9e8b2952987dba67380f6023084bc05
steve01.peng <steve01.peng@dongjie-peng.pjbest.com>
2016-06-02 17:20:30 +0800

    [修改] 修改check sql，另其符合标准。

--- sql/20160612/dmp/check/001_删除快递揽件录入页面_check.sql
+++ sql/20160612/dmp/check/001_删除快递揽件录入页面_check.sql
@@ -1,3 +1,11 @@
-SELECT * FROM `dmp40_function` WHERE `ID` = '504088'  AND  `functionname` = '快递揽件录入';	-- 没有记录
-
-SELECT count(*) FROM `dmp40_function` WHERE `ID` = '4010'  AND  `functionname` = '快递揽件录入'; -- 返回1
+SELECT
+  '删除原快递揽件录入页面'                                                                      AS '检查项',
+  IF((SELECT count(*)
+      FROM `dmp40_function`
+      WHERE `ID` = '504088' AND `functionname` = '快递揽件录入') < 1, 'success', 'failed') AS '执行结果'
+UNION
+SELECT
+  '揽收运单补录页面更名'                                                                     AS '检查项',
+  IF((SELECT count(*)
+      FROM `dmp40_function`
+      WHERE `ID` = '4010' AND `functionname` = '快递揽件录入') = 1, 'success', 'failed') AS '执行结果';
