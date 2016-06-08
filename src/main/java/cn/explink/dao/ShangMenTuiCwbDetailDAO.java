ec5ea408c46e1ddd2ed11468d6e50aca6263247a
Administrator <Administrator@HpsConcerto>
2016-06-08 16:48:12 +0800

    解决上门退订单打印问题

--- src/main/java/cn/explink/dao/ShangMenTuiCwbDetailDAO.java
+++ src/main/java/cn/explink/dao/ShangMenTuiCwbDetailDAO.java
@@ -108,8 +108,9 @@ public class ShangMenTuiCwbDetailDAO {
 		}
 		StringBuffer sql = new StringBuffer();
 	    sql.append("select distinct sd.cwb from shangmentuicwb_detail sd left join express_ops_cwb_detail cd "
-				+ " on sd.cwb=cd.cwb where cd.state=1 and cd.flowordertype IN(1,2,3,4,6,7,8,37) "); // hps_Concerto 只统计 导入数据 1 提货  ....
+				+ " on sd.cwb=cd.cwb where cd.state=1 ");
 		if (printType == 0) {
+			sql.append(" and cd.flowordertype IN(1,2,3,4,6,7,8,37) ");
 			sql.append(" and cd.printtime='' ");
 		} else {
 			sql.append(" and cd.printtime >= '" + begindate + "'  and cd.printtime <= '" + enddate + "'");
