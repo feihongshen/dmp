845dede42a26d21f064becb457106faa81780c93
gordon.zhou <gordon.zhou@vipshop.com>
2016-06-25 12:00:32 +0800

    修改脚本

---
+++ sql/V4.2.16/dmp/check/001.资金归集代扣_check_20160617.sql
@@ -0,0 +1,39 @@
+ALTER TABLE `fn_org_recharges`   
+  DROP COLUMN `current_model`,
+  DROP COLUMN `recharge_source`,
+  DROP COLUMN `cwb`,
+  DROP COLUMN `vpal_record_id`;
+
+drop TABLE `fn_orgrpt_order_recharge`;
+
+drop TABLE `fn_org_recharges_adjustment_record`;
+
+ALTER TABLE `fn_station_sign_order_details_snapshot`   
+  DROP COLUMN `shouldfareflag`,
+  DROP COLUMN `receivablefeeflag`,
+  DROP COLUMN `expressfreightflag`,
+  DROP COLUMN `shouldfare_receipt`,
+  DROP COLUMN `receivablefee_receipt`,
+  DROP COLUMN `expressfreight_receipt`;
+ 
+-- 现付类型快递运费订单快照表（fn_station_sign_order_details_snapshot_express）新增字段
+ALTER TABLE `fn_station_sign_order_details_snapshot_express`   
+  DROP COLUMN `shouldfareflag`,
+  DROP COLUMN `receivablefeeflag`,
+  DROP COLUMN `expressfreightflag`,
+  DROP COLUMN `shouldfare_receipt`,
+  DROP COLUMN `receivablefee_receipt`,
+  DROP COLUMN `expressfreight_receipt`;
+
+drop TABLE `fn_org_pay_account`;
+
+drop TABLE `fn_org_pay_account_modify_record`;
+
+drop TABLE `fn_vpal_interface`;
+
+drop TABLE `fn_vpal_record`;
+
+drop TABLE `fn_vpal_check_filelist`;
+
+drop TABLE `fn_set_bank`;
+
