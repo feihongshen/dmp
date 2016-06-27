0ad68b733cea7ffd9e7224029059dd9c07ea3449
jian.xie <jian.xie@vipshop.com>
2016-06-27 16:11:51 +0800

    【快递二期】快递临时表添加字段脚本。

---
+++ sql/V4.2.14/002_临时表alter_table_express_ops_cwb_exprss_detail_temp_20160627.sql
@@ -0,0 +1,2 @@
+alter table express_ops_cwb_exprss_detail_temp 
+add column return_credit decimal(18,2) NOT NULL DEFAULT '0.00' COMMENT '”¶ÕÀ‘À∑—';
---
+++ sql/V4.2.14/rollback/002_临时表express_ops_cwb_exprss_detail_temp添加字段_rollback.sql
@@ -0,0 +1,2 @@
+alter table express_ops_cwb_exprss_detail_temp 
+DROP column return_credit;
