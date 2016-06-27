b0bd541d0095b4fe38e6eb8f077dc2583cb4e957
zhouhuan <huan.zhou@pjbest.com>
2016-06-27 18:43:05 +0800

    【修改】修个tpsMq定时器不起作用bug【周欢】

--- src/main/java/cn/explink/b2c/tools/B2cEnum.java
+++ src/main/java/cn/explink/b2c/tools/B2cEnum.java
@@ -114,7 +114,7 @@ public enum B2cEnum {
 	Shenzhoushuma(20137,"神州数码","shenzhoushuma",0),
 	ZheMeng_track(23001, "哲盟_轨迹", "zhemeng_track", 0),
 	EMS(22010, "邮政速递", "EMS", 0), 
-	TPS_MQ(22501, "tps订单下发接口", "tps_mq", 0);
+	TPSVipShop_MQ(22501, "tps订单下发接口", "tps_mq", 0);
 
 	 
 
