de089c13fe88f5f9a91aabc88cdd0bba5256923d
jian.xie <jian.xie@vipshop.com>
2016-06-28 18:22:51 +0800

    修改脚本编码

--- sql/V4.2.14/002_临时表alter_table_express_ops_cwb_exprss_detail_temp_20160627.sql
+++ sql/V4.2.14/002_临时表alter_table_express_ops_cwb_exprss_detail_temp_20160627.sql
@@ -1,3 +1,3 @@
 alter table express_ops_cwb_exprss_detail_temp 
-add column return_credit decimal(18,2) NOT NULL DEFAULT '0.00' COMMENT '应退运费',
-add column order_source int(11) NOT NULL default '0' comment '订单来源';
+add column return_credit decimal(18,2) NOT NULL DEFAULT '0.00' COMMENT '搴旈��杩愯垂',
+add column order_source int(11) NOT NULL default '0' comment '璁㈠崟鏉ユ簮';
