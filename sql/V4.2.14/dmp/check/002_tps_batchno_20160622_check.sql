63df5f623afb2f511ced06d73e3e98322eb953a7
warrior.wang <warrior.wang@vipshop.com>
2016-06-30 10:33:06 +0800

    整理上线sql

--- sql/V4.2.14/dmp/check/002_tps_batchno_20160622_check.sql
+++
@@ -1,4 +0,0 @@
-SELECT '001_tps_batchno_20160622' AS '脚本文件名',IF((SELECT COUNT(1) FROM express_auto_param_config where name='MQ_REC_TPS_BATCHNO_CHANNEL' = 1),'success','failed') AS '执行结果'
-UNION ALL
-SELECT '001_tps_batchno_20160622' AS '脚本文件名',IF((SELECT COUNT(1) FROM express_auto_param_config where name='MQ_REC_TPS_BATCHNO_QUEUE' = 1),'success','failed') AS '执行结果'
-;
