f7503805d4a6c7726c9117f295b33b5a0ff357e5
wuqiang.liu <wangxufei668@163.com>
2016-07-14 15:00:34 +0800

    回退转业务快递定时器删除的所有修改

--- sql/20160714/dmp/001_删除定时任务doTime_expressTransOrder.sql
+++
@@ -1,4 +0,0 @@
-delete from inf_q_blob_triggers where trigger_name='doTime_expressTransOrder';
-SET FOREIGN_KEY_CHECKS = 0; 
-delete from inf_q_triggers where trigger_name='doTime_expressTransOrder';
-SET FOREIGN_KEY_CHECKS = 1; 
