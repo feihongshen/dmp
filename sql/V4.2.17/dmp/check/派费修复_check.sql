3aa0cd947912adf43a2291b38d6bc2254950eaa5
彭东杰 <steve01.peng@pjbest.com>
2016-08-05 14:13:00 +0800

    【修改】增加一些索引。

--- sql/V4.2.17/dmp/check/派费修复_check.sql
+++ sql/V4.2.17/dmp/check/派费修复_check.sql
@@ -102,4 +102,144 @@ SELECT
   IF((SELECT COUNT(1)
       FROM INFORMATION_SCHEMA.STATISTICS
       WHERE TABLE_NAME = 'fn_df_confirm_rate_detail' AND INDEX_NAME = 'detail_confirm_rate_id_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_order_no_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_order_no_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_charge_type_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_charge_type_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_deliverybranchid_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_deliverybranchid_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_deliver_id_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_deliver_id_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_customerid_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_customerid_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_emaildate_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_emaildate_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_credate_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_credate_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_pick_time_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_pick_time_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_mobilepodtime_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_mobilepodtime_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_org_auditingtime_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_org' AND INDEX_NAME = 'adjustment_org_auditingtime_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_order_no_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_order_no_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_charge_type_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_charge_type_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_deliverybranchid_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_deliverybranchid_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_deliver_id_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_deliver_id_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_customerid_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_customerid_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_emaildate_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_emaildate_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_credate_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_credate_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_pick_time_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_pick_time_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_mobilepodtime_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_mobilepodtime_idx') > 0, 'success',
+     'failed')                         AS '执行结果'
+UNION ALL
+SELECT
+  'check adjustment_staff_auditingtime_idx' AS '脚本注释',
+  IF((SELECT COUNT(1)
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE TABLE_NAME = 'fn_df_fee_adjustment_staff' AND INDEX_NAME = 'adjustment_staff_auditingtime_idx') > 0, 'success',
      'failed')                         AS '执行结果';
--- sql/V4.2.17/dmp/派费修复.sql
+++ sql/V4.2.17/dmp/派费修复.sql
@@ -18,3 +18,25 @@ ALTER TABLE `fn_df_confirm_rate` ADD INDEX `confirm_rate_kpi_result_idx` (`kpi_r
 ALTER TABLE `fn_df_confirm_rate` ADD INDEX `confirm_rate_charger_type_idx` (`charger_type`);
 --add indexes for fn_df_confirm_rate_detail
 ALTER TABLE `fn_df_confirm_rate_detail` ADD INDEX `detail_confirm_rate_id_idx` (`confirm_rate_id`);
+--add indexes for fn_df_fee_adjustment_org
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_order_no_idx` (`order_no`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_charge_type_idx` (`charge_type`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_deliverybranchid_idx` (`deliverybranchid`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_deliver_id_idx` (`deliver_id`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_customerid_idx` (`customerid`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_emaildate_idx` (`emaildate`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_credate_idx` (`credate`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_pick_time_idx` (`pick_time`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_mobilepodtime_idx` (`mobilepodtime`);
+ALTER TABLE `fn_df_fee_adjustment_org` ADD INDEX `adjustment_org_auditingtime_idx` (`auditingtime`);
+--add indexes for fn_df_fee_adjustment_staff
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_order_no_idx` (`order_no`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_charge_type_idx` (`charge_type`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_deliverybranchid_idx` (`deliverybranchid`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_deliver_id_idx` (`deliver_id`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_customerid_idx` (`customerid`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_emaildate_idx` (`emaildate`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_credate_idx` (`credate`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_pick_time_idx` (`pick_time`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_mobilepodtime_idx` (`mobilepodtime`);
+ALTER TABLE `fn_df_fee_adjustment_staff` ADD INDEX `adjustment_staff_auditingtime_idx` (`auditingtime`);
