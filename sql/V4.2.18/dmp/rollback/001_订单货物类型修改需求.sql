cac10261919aa1ee6372b1abaedbf300e698d74a
chunlei05.li <chunlei05.li@pjbest.com>
2016-08-29 11:58:23 +0800

    4.2.18版本sql整理

--- sql/V4.2.18/dmp/rollback/001_订单货物类型修改需求.sql
+++ sql/V4.2.18/dmp/rollback/001_订单货物类型修改需求.sql
@@ -1,10 +1,6 @@
 -- 创建表,用于存放货物类型修改申请的数据
 drop table  express_ops_applyeditcartype ;
 
-
--- 修改订单主表的货物类型字段，默认值改为'普件'
-alter table express_ops_cwb_detail alter column cartype set DEFAULT null;
-
 -- 添加新页面菜单
 delete from dmp40_function where id=602566 ;
 delete from dmp40_function where id=602567 ;
---
+++ sql/V4.2.18/dmp/rollback/003_online_ddl_rollback.sql
@@ -0,0 +1,2 @@
+-- 修改订单主表的货物类型字段，默认值改为'普件'
+alter table express_ops_cwb_detail alter column cartype set DEFAULT null;
