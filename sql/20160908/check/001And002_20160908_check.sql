d55ab1a4c10fb8af83b36152a3cae391ff9fb734
jian.xie <jian.xie@vipshop.com>
2016-09-09 17:11:05 +0800

    修改0908的脚本

--- sql/20160908/check/001And002_20160908_check.sql
+++ sql/20160908/check/001And002_20160908_check.sql
@@ -1,4 +1,3 @@
-SELECT '001' AS '脚本注释', IF((SELECT count(1) from information_schema.`COLUMNS` where TABLE_NAME='express_set_user_inf' and COLUMN_NAME='create_date' and EXTRA='' or EXTRA is null) = 1,'success','failed') AS '执行结果'
+SELECT '001' AS '脚本注释', IF((SELECT count(1) from information_schema.`COLUMNS` where TABLE_SCHEMA = schema() AND TABLE_NAME='express_set_user_inf' and COLUMN_NAME='create_date' and (EXTRA='' or EXTRA is null)) = 1,'success','failed') AS '执行结果'
 UNION ALL
-SELECT '002' AS '脚本注释',IF((SELECT count(1) from information_schema.`COLUMNS` where TABLE_NAME='express_set_branch_inf' and COLUMN_NAME='create_date' and EXTRA='' or EXTRA is null) =1,'success','failed') AS '执行结果'
-;
+SELECT '002' AS '脚本注释',IF((SELECT count(1) from information_schema.`COLUMNS` where TABLE_SCHEMA = schema() AND TABLE_NAME='express_set_branch_inf' and COLUMN_NAME='create_date' and (EXTRA='' or EXTRA is null)) =1,'success','failed') AS '执行结果';
