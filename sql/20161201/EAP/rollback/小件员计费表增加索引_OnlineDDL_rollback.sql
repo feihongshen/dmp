4f8d35b2fd9926c03acf17a79a1d869edfc49660
彭东杰 <steve01.peng@pjbest.com>
2016-11-24 17:45:47 +0800

    补上sql

---
+++ sql/20161201/EAP/rollback/小件员计费表增加索引_OnlineDDL_rollback.sql
@@ -0,0 +1,2 @@
+DROP INDEX `fee_staff_is_calculted_idx` ON `fn_df_fee_staff`;
+DROP INDEX `fee_org_is_calculted_idx` ON `fn_df_fee_org`;
---
+++ sql/20161201/EAP/小件员计费表增加索引_OnlineDDL.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `fn_df_fee_staff` ADD INDEX `fee_staff_is_calculted_idx` (`is_calculted`);
+ALTER TABLE `fn_df_fee_org` ADD INDEX `fee_org_is_calculted_idx` (`is_calculted`);
