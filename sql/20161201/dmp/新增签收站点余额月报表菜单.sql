3eeda6bb37cbe53cada4facca57123fcfcc207df
xiaofeng03.zhu <xiaofeng03.zhu@pjbest.com>
2016-11-25 18:03:01 +0800

    增加判断临时表tmp_dmp40_function_id是否存在

--- sql/20161201/dmp/新增签收站点余额月报表菜单.sql
+++ sql/20161201/dmp/新增签收站点余额月报表菜单.sql
@@ -1,6 +1,6 @@
-drop table tmp_dmp40_function_id;
+drop table if EXISTS tmp_dmp40_function_id;
 create table tmp_dmp40_function_id (id VARCHAR(64));
-insert into tmp_dmp40_function_id (id)  select max(ID)+1 from dmp40_function where parentfunctionid=8030;
+insert into tmp_dmp40_function_id (id)  select max(ID)+10 from dmp40_function where parentfunctionid=8030;
 
 INSERT INTO `dmp40_function` (
 	`ID`,
