4b37d4a128239cf6b791bb06a59a659cc70217d0
xiaofeng03.zhu <xiaofeng03.zhu@pjbest.com>
2016-11-26 10:41:29 +0800

    将保存最大菜单ID值方式由保存到临时表改成变量的方式

--- sql/20161201/dmp/新增签收站点余额月报表菜单.sql
+++ sql/20161201/dmp/新增签收站点余额月报表菜单.sql
@@ -1,7 +1,4 @@
-drop table if EXISTS tmp_dmp40_function_id;
-create table tmp_dmp40_function_id (id VARCHAR(64));
-insert into tmp_dmp40_function_id (id)  select max(ID)+10 from dmp40_function where parentfunctionid=8030;
-
+set @tmp_dmp40_function_id=(select max(ID)+10 from dmp40_function where parentfunctionid=8030);
 INSERT INTO `dmp40_function` (
 	`ID`,
 	`functionlevel`,
@@ -12,11 +9,10 @@ INSERT INTO `dmp40_function` (
 )
 VALUES
 	(
-		(select id from tmp_dmp40_function_id),
+		@tmp_dmp40_function_id,
 		'2',
 		'签收站点余额月报表',
-		(select id from tmp_dmp40_function_id),
+		@tmp_dmp40_function_id,
 		'${eapUrl}reportMonthForm.do?index&',
 		'8030'
 	);
-drop table tmp_dmp40_function_id;
