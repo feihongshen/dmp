4cdbfb5d2d87fba8ed7ddfea0da17cc80a3c7131
彭东杰 <steve01.peng@pjbest.com>
2016-11-30 13:52:11 +0800

    添加金蝶需要的参数

--- sql/20161201/EAP/001_账单金蝶对接.sql
+++ sql/20161201/EAP/001_账单金蝶对接.sql
@@ -17,7 +17,7 @@ CREATE TABLE `fn_msg_record` (
   KEY `fn_msg_record_interface_idx` (`intf`),
   KEY `fn_msg_record_status_idx` (`status`),
   KEY `fn_msg_record_send_or_recv_idx` (`send_or_recv`),
-  KEY `fn_msg_record_create_time_idx` (`create_time`) USING BTREE,
+  KEY `fn_msg_record_create_time_idx` (`create_time`),
   KEY `fn_msg_record_suc_or_fail_idx` (`suc_or_fail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -26,3 +26,16 @@ VALUES (306012, 2, 'EAP异常', 306012, '${eapUrl}msgRecord.do?index&', 3060);
 
 alter table `fn_cust_pay_report_cfg` add (`is_collect_deduction` tinyint not null default FALSE);
 ALTER TABLE `fn_cust_pay_report_cfg` ADD INDEX `is_collect_deduction_idx` (`is_collect_deduction`);
+
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_LOGIN_DB_TYPE', '0', '金蝶登录DB类型');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_LOGIN_LANGUAGE', 'L2', '金蝶登录LANGUAGE');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_LOGIN_DC_NAME', 'PJ002', '金蝶登录DC名');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_LOGIN_SLN_NAME', 'eas', '金蝶登录SLN名');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_PASSWORD', 'dmp123456', '金蝶系统登录密码');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_USERNAME', 'DMP', '金蝶系统登录名');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_RAMDOM', 'PJBEST01', '发送给金蝶消息加密的向量');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_SECRET', 'PJBEST01', '发送给金蝶消息加密的key');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('KINGDEE_URL_FOR_EAS', 'http://172.30.14.11:6888/ormrpc/services/WSPJOtherBillFacade?wsdl', '金蝶提供的接受账单的地址');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('EAS_KINGDEE_INT_ENABLE', 'false', '落地配跟金蝶接口开关');
+INSERT INTO `express_set_system_install` (`name`, `value`, `chinesename`) VALUES ('EAS_PERSOPN_IN_KINGDEE', 'xue01.wan', '落地配在金蝶系统的职员编号');
+
--- sql/20161201/EAP/check/001_账单金蝶对接_check.sql
+++ sql/20161201/EAP/check/001_账单金蝶对接_check.sql
@@ -1,7 +1,30 @@
 SELECT '新建eap信息重推表' AS '脚本文件名',IF((SELECT COUNT(1) FROM information_schema.tables WHERE table_schema = schema() and table_name = 'fn_msg_record') > 0,'success','failed') AS '执行结果'
 UNION ALL
-SELECT '新增EAP异常菜单' AS '脚本文件名',IF((SELECT COUNT(1) FROM dmp40_function WHERE ID = '306012' AND functionname='EAP异常')= 1 ,'success','failed') AS '执行结果'
+SELECT '新增EAP异常菜单' AS '脚本文件名',IF((SELECT COUNT(1) FROM dmp40_function WHERE ID = '306012' AND functionname='EAP异常') = 1 ,'success','failed') AS '执行结果'
 UNION ALL
-SELECT '新增是否抵扣字段' AS '脚本注释', IF((SELECT count(1) from information_schema.`COLUMNS` where TABLE_NAME='fn_cust_pay_report_cfg' and COLUMN_NAME='is_collect_deduction') = 1,'success','failed') AS '执行结果'
+SELECT '新增是否抵扣字段' AS '脚本注释', IF((SELECT count(1) from information_schema.`COLUMNS` where TABLE_NAME='fn_cust_pay_report_cfg' and COLUMN_NAME='is_collect_deduction') > 0,'success','failed') AS '执行结果'
 UNION ALL
-SELECT '新增是否抵扣字段索引' AS '脚本注释', IF((SELECT COUNT(1) FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_NAME = 'fn_cust_pay_report_cfg' AND INDEX_NAME = 'is_collect_deduction_idx') = 1, 'success', 'failed') AS '执行结果'
+SELECT '新增是否抵扣字段索引' AS '脚本注释', IF((SELECT COUNT(1) FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_NAME = 'fn_cust_pay_report_cfg' AND INDEX_NAME = 'is_collect_deduction_idx') > 0, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_LOGIN_DB_TYPE' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_LOGIN_DB_TYPE') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_LOGIN_LANGUAGE' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_LOGIN_LANGUAGE') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_LOGIN_DC_NAME' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_LOGIN_DC_NAME') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_LOGIN_SLN_NAME' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_LOGIN_SLN_NAME') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_PASSWORD' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_PASSWORD') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_USERNAME' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_USERNAME') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_RAMDOM' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_RAMDOM') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_SECRET' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_SECRET') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段KINGDEE_URL_FOR_EAS' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'KINGDEE_URL_FOR_EAS') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段EAS_KINGDEE_INT_ENABLE' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'EAS_KINGDEE_INT_ENABLE') = 1, 'success', 'failed') AS '执行结果'
+UNION ALL
+SELECT '新增字段EAS_PERSOPN_IN_KINGDEE' AS '脚本注释', IF((SELECT COUNT(1) FROM express_set_system_install WHERE name = 'EAS_PERSOPN_IN_KINGDEE') = 1, 'success', 'failed') AS '执行结果'
+
