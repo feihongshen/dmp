0cc71a6fcf94b6605a5c479968900b3f7a874427
shenhf <shenhf@explink.cn>
2017-03-21 18:04:47 +0800

    [新增]快递单出站，快递单确认sql

--- src/main/db/sql/002_20170310/01_auto_20170310.sql
+++ src/main/db/sql/002_20170310/01_auto_20170310.sql
@@ -1,1 +1,3 @@
 INSERT INTO `express_set_menu_info_new` ( `menuname`, `menuurl`, `menulevel`, `menuno`, `parentno`, `menuimage`, `adminshowflag`) VALUES ( '快递单录入', NULL, 'p', 'P30', '-1', NULL, NULL);
+INSERT INTO `express_set_menu_info_new` ( `menuname`, `menuurl`, `menulevel`, `menuno`, `parentno`, `menuimage`, `adminshowflag`) VALUES ( '快递揽件出站', NULL, 'p', 'P31', '-1', NULL, NULL);
+INSERT INTO `express_set_menu_info_new` ( `menuname`, `menuurl`, `menulevel`, `menuno`, `parentno`, `menuimage`, `adminshowflag`) VALUES ( '快递揽件确认', NULL, 'p', 'P32', '-1', NULL, NULL);
